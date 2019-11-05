2b26f6d0d8a700a58d7866ad4a7259ee4b197e6b
Samiul Sk <sam91v@gmail.com>
2019-11-05 15:09:27 +0530

    first successful response

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
+
 import (
-	"net/http"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 func main() {
 
+	// default router from Gin
+	router := gin.Default()
+
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	api:= router.Group("/api")
+	{
+		api.GET("/", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+
+		})
+	}
+
+	_ = router.Run(":3000")
 }
