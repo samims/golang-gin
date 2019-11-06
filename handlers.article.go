d1a2001f0bdfdc119a5f203d4d878624ea7fe583
Samiul Sk <sam91v@gmail.com>
2019-11-06 14:07:50 +0530

    single renderer function for XML, JSON and HTML implemented

--- handlers.article.go
+++ handlers.article.go
@@ -8,23 +8,34 @@ import (
 
 func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
-	c.HTML(
-		http.StatusOK,
-		"index.html",
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles,
+	}, "index.html")
 }
 
+// detail view for single article
 func getArticle(c *gin.Context) {
-	if articleId, err := strconv.Atoi(c.Param("article_id")); err == nil {
-		if article, err := getArticleByID(articleId); err == nil {
+	// Check if the article ID is valid
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		// Check if the article exists
+		if article, err := getArticleByID(articleID); err == nil {
 			c.HTML(
 				http.StatusOK,
 				"article.html",
-				gin.H{"title": article.Title, "payload": article})
+				gin.H{
+					"title":   article.Title,
+					"payload": article,
+				},
+			)
+
+		} else {
+			// If the article is not found, abort with an error
+			_ = c.AbortWithError(http.StatusNotFound, err)
 		}
+
+	} else {
+		// If an invalid article ID is specified in the URL, abort with an error
+		c.AbortWithStatus(http.StatusNotFound)
 	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var router *gin.Engine
@@ -15,6 +16,22 @@ func main() {
 	initializeRoutes()
 
 	// Start serving the application
-	router.Run()
+	_ = router.Run()
 
+}
+
+// Rendering of HTML,JSON or XML based on "Accept" header
+// if header does not specify it will render html
+
+func render(c *gin.Context, data gin.H, templateName string) {
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		// Respond with Json
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/XML":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
+
+	}
 }
